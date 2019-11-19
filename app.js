6f91567f0381e1899996584e5e1d7783f24cf63a
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-19 06:41:05 +0000

    Add route

--- app.js
+++ app.js
@@ -21,16 +21,40 @@ const RestaurantInfo = require('./models/restaurant_info')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
+app.get('/restaurants', (req, res) => {
+  res.send('List all restaurant infos')
+})
 
+app.get('/restaurants/new', (req, res) => {
+  res.send('Show page of creating a restaurant info')
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurantInfo = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
   res.render('show', { restaurant: restaurantInfo })
-
 })
+
+app.post('/restaurants', (req, res) => {
+  res.send('Create a restaurant info')
+})
+
+app.get('/restaurants/:restaurant_id/edit', (req, res) => {
+  res.send('Show page of editing a restaurant info')
+})
+
+app.post('/restaurants/:restaurant_id/edit', (req, res) => {
+  res.send('Edit a restaurant info')
+})
+
+app.post('/restaurants/:restaurant_id/delete', (req, res) => {
+  res.send('Delete a restaurant info')
+})
+
 
 app.get('/search', (req, res) => {
 
@@ -41,9 +65,8 @@ app.get('/search', (req, res) => {
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
-
 app.use(express.static('public'))
 
 app.listen(3000, () => {
-  console.log(`Express is listening!`)
+  console.log(`Express app is listening!`)
 })
