b3079ef303a01cb5e528b8879b8dc6fc07b8f8d6
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-25 07:01:16 +0000

    Fix routes of sort with supporting query strings and adjust texts of select options

--- routes/sort.js
+++ routes/sort.js
@@ -2,38 +2,71 @@ const express = require('express')
 const router = express.Router()
 const RestaurantInfo = require('../models/restaurant_info')
 
-router.get('/ascending', (req, res) => {
-  RestaurantInfo.find({})
-    .sort({ name: 'asc' })
-    .exec((err, restaurantinfos) => {
-      if (err) return console.error(err)
-      return res.render('index', { restaurantinfos })
-    })
-})
+// router.get('/ascending', (req, res) => {
+//  RestaurantInfo.find({})
+//    .sort({ name: 'asc' })
+//    .exec((err, restaurantinfos) => {
+//      if (err) return console.error(err)
+//      return res.render('index', { restaurantinfos })
+//    })
+// })
+//
+// router.get('/descending', (req, res) => {
+//  RestaurantInfo.find({})
+//    .sort({ name: 'desc' })
+//    .exec((err, restaurantinfos) => {
+//      if (err) return console.error(err)
+//      return res.render('index', { restaurantinfos })
+//    })
+// })
+//
+// router.get('/category', (req, res) => {
+//  RestaurantInfo.find({})
+//    .sort({ category: 'asc' })
+//    .exec((err, restaurantinfos) => {
+//      if (err) return console.error(err)
+//      return res.render('index', { restaurantinfos })
+//    })
+// })
+//
+// router.get('/location', (req, res) => {
+//  RestaurantInfo.find({})
+//    .sort({ location: 'asc' })
+//    .exec((err, restaurantinfos) => {
+//      if (err) return console.error(err)
+//      return res.render('index', { restaurantinfos })
+//    })
+// })
 
-router.get('/descending', (req, res) => {
+router.get('/name', (req, res) => {
+  const order = req.query.order
+  const sortObject = { name: order}
   RestaurantInfo.find({})
-    .sort({ name: 'desc' })
+    .sort(sortObject)
     .exec((err, restaurantinfos) => {
-      if (err) return console.error(err)
+      if (err) return res.sendStatus(500)
       return res.render('index', { restaurantinfos })
     })
 })
 
 router.get('/category', (req, res) => {
-  RestaurantInfo.find({})
-    .sort({ category: 'asc' })
+  const order = req.query.order
+  const sortObject = { category: order}
+  RestaurantInfo.find()
+    .sort(sortObject)
     .exec((err, restaurantinfos) => {
-      if (err) return console.error(err)
+      if (err) return res.sendStatus(500)
       return res.render('index', { restaurantinfos })
     })
 })
 
 router.get('/location', (req, res) => {
-  RestaurantInfo.find({})
-    .sort({ location: 'asc' })
+  const order = (req.query.order).toString()
+  const sortObject = { location: order}
+  RestaurantInfo.find()
+    .sort(sortObject)
     .exec((err, restaurantinfos) => {
-      if (err) return console.error(err)
+      if (err) return res.sendStatus(500)
       return res.render('index', { restaurantinfos })
     })
 })
