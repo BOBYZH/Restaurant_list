4a5254c49539d002fc5e25483b6d5615e1aadd05
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-26 03:42:31 +0000

    Fix sort functions to original version, use bootswatch, and preload packages for login system

--- routes/sort.js
+++ routes/sort.js
@@ -37,7 +37,7 @@ const RestaurantInfo = require('../models/restaurant_info')
 //      return res.render('index', { restaurantinfos })
 //    })
 // })
-
+// 
 router.get('/name', (req, res) => {
   const order = req.query.order
   const sortObject = { name: order}
@@ -70,5 +70,26 @@ router.get('/location', (req, res) => {
       return res.render('index', { restaurantinfos })
     })
 })
+
+//router.get('/', (req, res) => {
+//  let sortObject ={}
+//  let filter = req.query.filter
+//  const order = req.query.order
+//  console.log(filter, order)
+//  if (filter = 'name') {
+//    sortObject = { name: order}
+//  } else if (filter = 'category') {
+//    sortObject = { category: order}
+//  } else if (filter = 'location') {
+//    sortObject = { location: order}
+//  }
+//  console.log(sortObject)
+//  RestaurantInfo.find({})
+//    .sort(sortObject)
+//    .exec((err, restaurantinfos) => {
+//      if (err) return res.sendStatus(500)
+//      return res.render('index', { restaurantinfos })
+//    })
+//})
 
 module.exports = router
