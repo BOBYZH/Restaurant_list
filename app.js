065ae61117740c62d8948206975d5e8bbe200272
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-26 07:35:12 +0000

    Add user login feature using passport

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const passport = require('passport')
 
 mongoose.connect('mongodb://localhost/restaurantInfo', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -31,6 +32,16 @@ app.use(session({
   resave: false,
   saveUninitialized: true,
 }))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./config/passport')(passport)
+
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 app.use('/', require('./routes/home'))
 app.use('/restaurants', require('./routes/restaurants'))
