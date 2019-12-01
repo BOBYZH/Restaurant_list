bde0bcad0a1c0c3cacce93b76ef53f148653c714
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-12-01 07:09:31 +0000

    Use query strings to execute sort function with only one route

--- routes/sort.js
+++ routes/sort.js
@@ -4,94 +4,58 @@ const RestaurantInfo = require('../models/restaurant_info')
 
 const { authenticated } = require('../config/auth')
 
-// router.get('/ascending', (req, res) => {
-//  RestaurantInfo.find({})
-//    .sort({ name: 'asc' })
-//    .exec((err, restaurantinfos) => {
-//      if (err) return console.error(err)
-//      return res.render('index', { restaurantinfos })
-//    })
-// })
-//
-// router.get('/descending', (req, res) => {
-//  RestaurantInfo.find({})
-//    .sort({ name: 'desc' })
-//    .exec((err, restaurantinfos) => {
-//      if (err) return console.error(err)
-//      return res.render('index', { restaurantinfos })
-//    })
-// })
-//
-// router.get('/category', (req, res) => {
-//  RestaurantInfo.find({})
-//    .sort({ category: 'asc' })
-//    .exec((err, restaurantinfos) => {
-//      if (err) return console.error(err)
-//      return res.render('index', { restaurantinfos })
-//    })
-// })
-//
-// router.get('/location', (req, res) => {
-//  RestaurantInfo.find({})
-//    .sort({ location: 'asc' })
-//    .exec((err, restaurantinfos) => {
-//      if (err) return console.error(err)
-//      return res.render('index', { restaurantinfos })
-//    })
-// })
-//
-router.get('/name', authenticated, (req, res) => {
-  const order = req.query.order
-  const sortObject = { name: order }
-  RestaurantInfo.find({ userId: req.user._id })
-    .sort(sortObject)
-    .exec((err, restaurantinfos) => {
-      if (err) return res.sendStatus(500)
-      return res.render('index', { restaurantinfos })
-    })
-})
-
-router.get('/category', authenticated, (req, res) => {
-  const order = req.query.order
-  const sortObject = { category: order }
-  RestaurantInfo.find({ userId: req.user._id })
-    .sort(sortObject)
-    .exec((err, restaurantinfos) => {
-      if (err) return res.sendStatus(500)
-      return res.render('index', { restaurantinfos })
-    })
-})
-
-router.get('/location', authenticated, (req, res) => {
-  const order = (req.query.order).toString()
-  const sortObject = { location: order }
-  RestaurantInfo.find({ userId: req.user._id })
-    .sort(sortObject)
-    .exec((err, restaurantinfos) => {
-      if (err) return res.sendStatus(500)
-      return res.render('index', { restaurantinfos })
-    })
-})
-
-// router.get('/', (req, res) => {
-//  let sortObject ={}
-//  let filter = req.query.filter
+//router.get('/name', authenticated, (req, res) => {
 //  const order = req.query.order
-//  console.log(filter, order)
-//  if (filter = 'name') {
-//    sortObject = { name: order}
-//  } else if (filter = 'category') {
-//    sortObject = { category: order}
-//  } else if (filter = 'location') {
-//    sortObject = { location: order}
-//  }
-//  console.log(sortObject)
-//  RestaurantInfo.find({})
+//  const sortObject = { name: order }
+//  RestaurantInfo.find({ userId: req.user._id })
 //    .sort(sortObject)
 //    .exec((err, restaurantinfos) => {
 //      if (err) return res.sendStatus(500)
 //      return res.render('index', { restaurantinfos })
 //    })
-// })
+//})
+//
+//router.get('/category', authenticated, (req, res) => {
+//  const order = req.query.order
+//  const sortObject = { category: order }
+//  RestaurantInfo.find({ userId: req.user._id })
+//    .sort(sortObject)
+//    .exec((err, restaurantinfos) => {
+//      if (err) return res.sendStatus(500)
+//      return res.render('index', { restaurantinfos })
+//    })
+//})
+//
+//router.get('/location', authenticated, (req, res) => {
+//  const order = (req.query.order).toString()
+//  const sortObject = { location: order }
+//  RestaurantInfo.find({ userId: req.user._id })
+//    .sort(sortObject)
+//    .exec((err, restaurantinfos) => {
+//      if (err) return res.sendStatus(500)
+//      return res.render('index', { restaurantinfos })
+//    })
+//})
+
+ router.get('/', authenticated, (req, res) => {
+  let sortObject ={}
+  let filter = req.query.filter
+  const order = req.query.order
+  console.log(filter, order)
+  if (filter === 'name') {
+    sortObject = { name: order}
+  } else if (filter === 'category') {
+    sortObject = { category: order}
+  } else if (filter === 'location') {
+    sortObject = { location: order}
+  }
+  console.log(sortObject)
+  RestaurantInfo.find({ userId: req.user._id })
+    .sort(sortObject)
+    .exec((err, restaurantinfos) => {
+      if (err) return res.sendStatus(500)
+      return res.render('index', { restaurantinfos })
+    })
+ })
 
 module.exports = router
